a55cf44353b120ffebcc78163ef27a8192809e7c
nolink <1377737@qq.com>
2016-07-09 14:55:28 +0800

    add more images

--- src/shaxzc/main.go
+++ src/shaxzc/main.go
@@ -11,8 +11,26 @@ func index(w http.ResponseWriter, r *http.Request) {
 	t.Execute(w, nil)
 }
 
+func about(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("about.html")
+	t.Execute(w, nil)
+}
+
+func benefit(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("benefit.html")
+	t.Execute(w, nil)
+}
+
+func contrast(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("contrast.html")
+	t.Execute(w, nil)
+}
+
 func main() {
 	http.HandleFunc("/", index)
+	http.HandleFunc("/about", about)
+	http.HandleFunc("/benefit", benefit)
+	http.HandleFunc("/contrast", contrast)
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	err := http.ListenAndServe(":9090", nil)
 	if err != nil {
